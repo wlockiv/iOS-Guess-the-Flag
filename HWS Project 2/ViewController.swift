ba5c02b3b44ee522ef491afefab58dba9dc255c1
Walker <walker@projectdado.com>
2023-03-09 17:53:19 -0600

    Initial Commit

---
+++ HWS Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWS Project 2
+//
+//  Created by Walker Lockard on 3/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
